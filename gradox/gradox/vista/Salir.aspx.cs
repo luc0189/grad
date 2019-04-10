437f0b348461d23740caad570f9d17b979462226
LUIS CARLOS SANCHEZ CABRERA <luc0189@gmail.com>
2019-04-09 23:49:07 -0500

    algunos css y js mas el login

---
+++ gradox/gradox/vista/Salir.aspx.cs
@@ -0,0 +1,35 @@
+﻿using gradox.Controlador;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace gradox.vista
+{
+    public partial class Salir : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!Page.IsPostBack)
+            {
+                salirx();
+            }
+
+        }
+        public void salirx()
+        {
+
+            Session.Remove("USUARIO");
+            Session.Remove("perfil");
+            Session.Remove("NOMBRE");
+            Session.Remove("CC");
+            Session.Remove("ROL");
+            Session.Remove("BD");
+            Response.Redirect("../Login.aspx");
+
+        }
+    }
+}
